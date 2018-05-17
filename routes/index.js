e364a2ccf61b631e6ae52f682ab4571aaa174bb5
k <lookerkj@protonmail.com>
2018-05-17 15:25:22 -0700

    starting over

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render('views/index');
+    res.sendFile(path.join(__dirname + '/public/home.html'));
   })
 // app.get('/', (req, res) => {
 //   res.sendFile(path.join(__dirname + '/public/home.html'));
