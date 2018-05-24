d1ec63bce95dfb0bfe09fc821cda9031e8b0e816
k <lookerkj@protonmail.com>
2018-05-23 17:01:10 -0700

    DELEATING USLESS FILES

--- server.js
+++ server.js
@@ -15,77 +15,11 @@ var mongo = require('mongodb');
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/giftExApp');
 var db = mongoose.connection;
-//login end
+
 
 //init app
 const app = express();
 
-// //login
-// var routes = require('./routes/index');
-// var users = require('./routes/users');
-
-// // View Engine
-// app.set('views', path.join(__dirname, 'views'));
-// app.engine('handlebars', exphbs({defaultLayout:'layout'}));
-// app.set('view engine', 'handlebars');
-
-
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(cookieParser());
-
-// // Set Static Folder
-// app.use(express.static(path.join(__dirname, 'public')));
-
-// // Express Session
-// app.use(session({
-//   secret: 'secret',
-//   saveUninitialized: true,
-//   resave: true
-// }));
-
-// // Passport init
-// app.use(passport.initialize());
-// app.use(passport.session());
-
-// // Express Validator
-// app.use(expressValidator({
-//   errorFormatter: function(param, msg, value) {
-//       var namespace = param.split('.')
-//       , root    = namespace.shift()
-//       , formParam = root;
-
-//     while(namespace.length) {
-//       formParam += '[' + namespace.shift() + ']';
-//     }
-//     return {
-//       param : formParam,
-//       msg   : msg,
-//       value : value
-//     };
-//   }
-// }));
-
-// // Connect Flash
-// app.use(flash());
-
-// // Global Vars
-// app.use(function (req, res, next) {
-//   res.locals.success_msg = req.flash('success_msg');
-//   res.locals.error_msg = req.flash('error_msg');
-//   res.locals.error = req.flash('error');
-//   res.locals.user = req.user || null;
-//   next();
-// });
-
-// // app.use('/', routes);
-// app.use('/users', users);
-
-// //login end
-
-// //need to add mongoose to make it easier to talk to mongo
-// var mongoose = require('mongoose');
-// //I'm not sure about how to do this
 // mongoose.connect('mongodb://complicationlamp:gradschool1@ds129281.mlab.com:29281/gift-exchange-test');
 
 //this will contain the routeing for the server... ie run it locally
